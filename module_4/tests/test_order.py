c88b84439340182e7597df6ceb3695b8dc24004b
Randy Selimi <selimi.4@osu.edu>
2024-09-22 22:34:59 -0400

    feat(module_4): added marks

--- module_4/tests/test_order.py
+++ module_4/tests/test_order.py
@@ -5,7 +5,6 @@ from tests.test_pizza import example_pizza_simple_data, example_pizza_simple, ex
 @pytest.fixture
 def example_order_empty():
     return Order()
-
 @pytest.fixture
 def example_order_single(example_pizza_simple):
     pizzas = [example_pizza_simple]
@@ -14,7 +13,6 @@ def example_order_single(example_pizza_simple):
     order.pizzas = pizzas
     order.cost = cost
     return order
-
 @pytest.fixture
 def example_order_multiple(example_pizza_simple, example_pizza_complex, example_pizza_most_complex):
     pizzas = [example_pizza_simple, example_pizza_complex, example_pizza_most_complex]
@@ -24,52 +22,56 @@ def example_order_multiple(example_pizza_simple, example_pizza_complex, example_
     order.cost = cost
     return order
 
+@pytest.mark.order
 def test_order_init_empty(example_order_empty):
     newOrder = example_order_empty
     assert newOrder.pizzas == []
     assert newOrder.cost == 0
     assert newOrder.paid == False
-
+@pytest.mark.order
 def test_order_init_single(example_order_single, example_pizza_simple):
     newOrder = example_order_single
     assert newOrder.pizzas == [example_pizza_simple]
     assert newOrder.cost == 9
     assert newOrder.paid == False
-
+@pytest.mark.order
 def test_order_init_multiple(example_order_multiple, example_pizza_simple, example_pizza_complex, example_pizza_most_complex):
     newOrder = example_order_multiple
     assert newOrder.pizzas == [example_pizza_simple, example_pizza_complex, example_pizza_most_complex]
     assert newOrder.cost == 48
     assert newOrder.paid == False
 
+@pytest.mark.order
 def test_order_str_empty(example_order_empty):
     assert str(example_order_empty) == """Customer Requested:"""
-
+@pytest.mark.order
 def test_order_str_single(example_order_single):
     assert str(example_order_single) == """Customer Requested:Crust: thin, Sauce: ['marinara'], Cheese: mozzarella, Toppings: ['pepperoni'], Cost: 9"""
-
+@pytest.mark.order
 def test_order_str_multiple(example_order_multiple):
     assert str(example_order_multiple) == """Customer Requested:Crust: thin, Sauce: ['marinara'], Cheese: mozzarella, Toppings: ['pepperoni'], Cost: 9Crust: thick, Sauce: ['marinara', 'pesto'], Cheese: mozzarella, Toppings: ['pineapple', 'mushrooms'], Cost: 15Crust: gluten_free, Sauce: ['marinara', 'pesto', 'liv_sauce'], Cheese: mozzarella, Toppings: ['pepperoni', 'pineapple', 'mushrooms'], Cost: 24"""
-    
+
+@pytest.mark.order   
 def test_order_input_pizza_once(example_order_empty, example_pizza_simple_data):
     example_order_empty.input_pizza(example_pizza_simple_data["crust"], example_pizza_simple_data["sauce"], example_pizza_simple_data["cheese"], example_pizza_simple_data["toppings"])
     assert example_order_empty.cost == 9
-
+@pytest.mark.order
 def test_order_input_pizza_twice(example_order_empty, example_pizza_simple_data, example_pizza_complex_data):
     example_order_empty.input_pizza(example_pizza_simple_data["crust"], example_pizza_simple_data["sauce"], example_pizza_simple_data["cheese"], example_pizza_simple_data["toppings"])
     example_order_empty.input_pizza(example_pizza_complex_data["crust"], example_pizza_complex_data["sauce"], example_pizza_complex_data["cheese"], example_pizza_complex_data["toppings"])
     assert example_order_empty.cost == 24
-
+@pytest.mark.order
 def test_order_input_pizza_thrice(example_order_empty, example_pizza_simple_data, example_pizza_complex_data, example_pizza_most_complex_data):
     example_order_empty.input_pizza(example_pizza_simple_data["crust"], example_pizza_simple_data["sauce"], example_pizza_simple_data["cheese"], example_pizza_simple_data["toppings"])
     example_order_empty.input_pizza(example_pizza_complex_data["crust"], example_pizza_complex_data["sauce"], example_pizza_complex_data["cheese"], example_pizza_complex_data["toppings"])
     example_order_empty.input_pizza(example_pizza_most_complex_data["crust"], example_pizza_most_complex_data["sauce"], example_pizza_most_complex_data["cheese"], example_pizza_most_complex_data["toppings"])
     assert example_order_empty.cost == 48
 
+@pytest.mark.order
 def test_order_order_paid_empty(example_order_empty):
     example_order_empty.order_paid()
     assert example_order_empty.paid == True
-
+@pytest.mark.order
 def test_order_order_paid_multiple(example_order_multiple):
     example_order_multiple.order_paid()
     assert example_order_multiple.paid == True
--- module_4/tests/test_pizza.py
+++ module_4/tests/test_pizza.py
@@ -4,7 +4,6 @@ from source.pizza import Pizza
 @pytest.fixture
 def example_pizza_simple_data():
     return {"crust": "thin", "sauce": ["marinara"], "cheese": "mozzarella", "toppings": ["pepperoni"]}
-    
 @pytest.fixture
 def example_pizza_simple(example_pizza_simple_data):
     return Pizza(example_pizza_simple_data["crust"], example_pizza_simple_data["sauce"], example_pizza_simple_data["cheese"], example_pizza_simple_data["toppings"])
@@ -12,7 +11,6 @@ def example_pizza_simple(example_pizza_simple_data):
 @pytest.fixture
 def example_pizza_complex_data():
     return {"crust": "thick", "sauce": ["marinara", "pesto"], "cheese": "mozzarella", "toppings": ["pineapple", "mushrooms"]}
-    
 @pytest.fixture
 def example_pizza_complex(example_pizza_complex_data):
     return Pizza(example_pizza_complex_data["crust"], example_pizza_complex_data["sauce"], example_pizza_complex_data["cheese"], example_pizza_complex_data["toppings"])
@@ -20,11 +18,11 @@ def example_pizza_complex(example_pizza_complex_data):
 @pytest.fixture
 def example_pizza_most_complex_data():
     return {"crust": "gluten_free", "sauce": ["marinara", "pesto", "liv_sauce"], "cheese": "mozzarella", "toppings": ["pepperoni", "pineapple", "mushrooms"]}
-    
 @pytest.fixture
 def example_pizza_most_complex(example_pizza_most_complex_data):
     return Pizza(example_pizza_most_complex_data["crust"], example_pizza_most_complex_data["sauce"], example_pizza_most_complex_data["cheese"], example_pizza_most_complex_data["toppings"])
 
+@pytest.mark.pizza
 def test_pizza_init_simple(example_pizza_simple):
     newPizza = example_pizza_simple
     assert newPizza.crust == "thin"
@@ -32,7 +30,7 @@ def test_pizza_init_simple(example_pizza_simple):
     assert newPizza.cheese == "mozzarella"
     assert newPizza.toppings == ["pepperoni"]
     assert newPizza.cost() != 0
-
+@pytest.mark.pizza
 def test_pizza_init_complex(example_pizza_complex):
     newPizza = example_pizza_complex
     assert newPizza.crust == "thick"
@@ -41,15 +39,17 @@ def test_pizza_init_complex(example_pizza_complex):
     assert newPizza.toppings == ["pineapple", "mushrooms"]
     assert newPizza.cost() != 0
 
+@pytest.mark.pizza
 def test_pizza_str_simple(example_pizza_simple):
     assert str(example_pizza_simple) == """Crust: thin, Sauce: ['marinara'], Cheese: mozzarella, Toppings: ['pepperoni'], Cost: 9"""
-    
+@pytest.mark.pizza    
 def test_pizza_str_complex(example_pizza_most_complex):
     assert str(example_pizza_most_complex) == """Crust: gluten_free, Sauce: ['marinara', 'pesto', 'liv_sauce'], Cheese: mozzarella, Toppings: ['pepperoni', 'pineapple', 'mushrooms'], Cost: 24"""
 
+@pytest.mark.pizza
 def test_pizza_cost_simple(example_pizza_simple):
     assert example_pizza_simple.cost() == 9
-
+@pytest.mark.pizza
 def test_pizza_cost_complex(example_pizza_most_complex):
     assert example_pizza_most_complex.cost() == 24
 
